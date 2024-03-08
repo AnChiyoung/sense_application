562d4e6381173d76b4585a3bcc8df2f602d8e409
reichardcris <reichardcris@gmail.com>
2024-03-08 21:40:39 +0800

    user model

---
+++ sense_flutter_application/lib/models/user.dart
@@ -0,0 +1,48 @@
+class User {
+  final int id;
+  final String username;
+  final String email;
+  final String birthday;
+  final String phone;
+  final String gender;
+  final String ?profileImage;
+  final bool ?isSignup;
+  final bool ?isAddProfile;
+  final String ?description;
+  final Enum ?relationshipStatus;
+  final String ?mbti;
+  final bool isOwnCar;
+  final bool isPushAlarm;
+  final bool isMarketingAlarm;
+  final String ?stores;
+
+  User({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.birthday,
+    required this.phone,
+    required this.gender,
+    this.profileImage,
+    this.isSignup,
+    this.isAddProfile,
+    this.description,
+    this.relationshipStatus,
+    this.mbti,
+    this.isOwnCar = false,
+    this.isPushAlarm = false,
+    this.isMarketingAlarm = false,
+    this.stores,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      username: json['username'],
+      email: json['email'],
+      phone: json['phone'],
+      birthday: json['birthday'],
+      gender: json['gender']
+    );
+  }
+}
