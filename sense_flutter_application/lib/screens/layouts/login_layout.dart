99576a6b912d5a1507337dff001b09d7cb1e7378
reichardcris <reichardcris@gmail.com>
2024-03-12 01:09:51 +0800

    Login layout scafold update

--- sense_flutter_application/lib/screens/layouts/login_layout.dart
+++ sense_flutter_application/lib/screens/layouts/login_layout.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:go_router/go_router.dart';
 
 class LoginLayout extends ConsumerWidget {
 
   final String ?title;
   final Widget body;
   final Widget ?floating;
+  final Widget ?bottomNavigationBar;
   
-  const LoginLayout({super.key, required this.body, this.title, this.floating});
+  const LoginLayout({super.key, required this.body, this.title, this.floating, this.bottomNavigationBar});
   
   @override
   Widget build(BuildContext context, WidgetRef ref) {
 
+
     return Scaffold(
       appBar: AppBar(
-          leading: Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: SvgPicture.asset('lib/assets/images/icons/logo.svg')),
-          leadingWidth: 100,
+          // leading: SvgPicture.asset('lib/assets/images/icons/logo.svg'),
+          // leadingWidth: 100,
+          automaticallyImplyLeading: true,
           shape: const Border(
             bottom: BorderSide(
               width:1,
@@ -30,7 +31,8 @@ class LoginLayout extends ConsumerWidget {
       body: GestureDetector(
         onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
         child: body,
-      )
+      ),
+      bottomNavigationBar: bottomNavigationBar,
     );
   }
 }
