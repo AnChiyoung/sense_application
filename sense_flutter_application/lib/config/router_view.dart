fff3335765332ce48ecabd3e26cf99c5d1ef6cb0
reichardcris <reichardcris@gmail.com>
2024-03-13 00:18:38 +0800

    update router view dynamic routing

--- sense_flutter_application/lib/config/router_view.dart
+++ sense_flutter_application/lib/config/router_view.dart
@@ -3,6 +3,8 @@ import 'package:sense_flutter_application/screens/login_screen.dart';
 import 'package:sense_flutter_application/screens/signup_screen/index.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sense_flutter_application/providers/auth/policy_provider.dart';
 
 class RouterView extends StatelessWidget {
 
@@ -11,29 +13,26 @@ class RouterView extends StatelessWidget {
   final _router = GoRouter(
     routes: [
       GoRoute(path: '/', redirect: (context, state) {
-        return Future.value('/auth');
+        return Future.value('/login');
       }),
-      GoRoute(path: '/auth', 
+      GoRoute(path: '/login', 
         builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        name: 'signup',
+        path: '/signup',
+        redirect: (context, state) {
+          return null;
+        },
         routes: [
           GoRoute(
-            name: 'signup',
-            path: 'signup',
-            redirect: (context, state) {
-              print(state.topRoute?.name);
-              // return ''
-            },
-            routes: [
-              GoRoute(
-                path: 'step1',
-                builder: (context, state) => const Step1(),
-              ),
-              GoRoute(
-                path: 'step2',
-                builder: (context, state) => const Step2(),
-              ),
-            ]
-          )
+            path: 'step1',
+            builder: (context, state) => const Step1(),
+          ),
+          GoRoute(
+            path: 'step2',
+            builder: (context, state) => const Step2(),
+          ),
         ]
       ),
       GoRoute(
