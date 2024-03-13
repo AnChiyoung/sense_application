7a1a4471e3eb4ef8c9171d4d478cb18d474b0bd4
reichardcris <reichardcris@gmail.com>
2024-03-13 22:50:40 +0800

    added addional providers

--- sense_flutter_application/lib/providers/auth/register_provider.dart
+++ sense_flutter_application/lib/providers/auth/register_provider.dart
@@ -14,6 +14,89 @@ final emailErrorProvider = StateProvider<String?>((ref) {
   return null;
 });
 
-final isEmailAvailable = StateProvider<bool>((ref) {
-  return false;
+final isEmailAvailableProvider = StateProvider<bool?>((ref) {
+  return null;
+});
+
+final isObscureProvider1 = StateProvider<bool>((ref) {
+  return true;
+});
+
+final isObscureProvider2 = StateProvider<bool>((ref) {
+  return true;
+});
+
+final passwordInputProvider = StateProvider<String>((ref) {
+  return '';
+});
+
+final confirmPasswordInputProvider = StateProvider<String>((ref) {
+  return '';
+});
+
+final errorPasswordProvider = StateProvider<String?>((ref) {
+  String pwd = ref.watch(passwordInputProvider);
+  RegExp passwordRegex = RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$');
+  
+  if (pwd.isEmpty) {
+    return null;
+  } else if (!passwordRegex.hasMatch(pwd)) {
+    return 'Please enter at least 8 characters with a combination of letters and numbers.';
+  }
+  else if (pwd.length < 8) {
+    return 'Password must be at least 8 characters long';
+  }
+  
+  return null;
+});
+
+final confirmPasswordErrorProvider = StateProvider<String?>((ref) {
+  String pwd = ref.watch(passwordInputProvider);
+  String confirmPwd = ref.watch(confirmPasswordInputProvider);
+  
+  if (confirmPwd.isEmpty) {
+    return null;
+  } else if (pwd != confirmPwd) {
+    return '비밀번호가 일치하지 않습니다';
+  }
+  
+  return null;
+
+});
+
+final nameInputProvider = StateProvider<String>((ref) {
+  return '';
+});
+
+final errorNameProvider = StateProvider<String?>((ref) {
+  String name = ref.watch(nameInputProvider);
+  RegExp regex = RegExp(r'^[가-힣]{2,7}$');
+  
+  if (name.isEmpty) {
+    return null;
+  } else if(regex.hasMatch(name)) {
+    return '한글 이름을 입력해주세요';
+  }
+  else if (name.length < 2) {
+    return '이름은 2자 이상이어야 합니다';
+  }
+  
+  return null;
+});
+
+enum Gender {male, female}
+
+final genderProvider = StateProvider<Gender?>((ref) {
+  return;
+});
+
+final selectedGender = StateProvider<String?>((ref) {
+  switch (ref.watch(genderProvider)) {
+    case  Gender.male:
+      return 'MALE';
+    case Gender.female:
+      return 'FEMALE';
+    default:
+      return '';
+  }
 });
