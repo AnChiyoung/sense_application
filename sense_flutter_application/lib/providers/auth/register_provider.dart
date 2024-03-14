8a770c307d626f6eff5f3025775921d92c728b53
reichardcris <reichardcris@gmail.com>
2024-03-14 20:33:38 +0800

    register provider update

--- sense_flutter_application/lib/providers/auth/register_provider.dart
+++ sense_flutter_application/lib/providers/auth/register_provider.dart
@@ -1,10 +1,13 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:sense_flutter_application/apis/auth/auth_api.dart';
 import 'package:sense_flutter_application/models/user.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sense_flutter_application/utils/utils.dart';
 
-final checkMailRepositoryProvider = StateProvider<AuthApi>((ref) {
+final authRepositoryProvider = StateProvider<AuthApi>((ref) {
   return AuthApi();
 });
+
 
 final emailInputProvider = StateProvider<String>((ref) {
   return '';
@@ -70,15 +73,17 @@ final nameInputProvider = StateProvider<String>((ref) {
 
 final errorNameProvider = StateProvider<String?>((ref) {
   String name = ref.watch(nameInputProvider);
-  RegExp regex = RegExp(r'^[가-힣]{2,7}$');
+  RegExp regex = RegExp(r'^[\uAC00-\uD7AF]+$');
   
   if (name.isEmpty) {
     return null;
-  } else if(regex.hasMatch(name)) {
-    return '한글 이름을 입력해주세요';
+  } else if(!regex.hasMatch(name)) {
+    return '김텍스트이름은 한글로 2자 이상 8자 미만으로 입력해 주세요';
   }
   else if (name.length < 2) {
-    return '이름은 2자 이상이어야 합니다';
+    return '이름은 한글로 2자 이상 입력해주세요.';
+  } else if (name.length > 8) {
+    return '영문과 숫자를 조합하여 최대 8자까지 입력해주세요.';
   }
   
   return null;
@@ -99,4 +104,61 @@ final selectedGender = StateProvider<String?>((ref) {
     default:
       return '';
   }
+});
+
+final dateOfBirthProvider = StateProvider<String>((ref) {
+  return '';
+});
+
+final phoneInputProvider = StateProvider<String>((ref) {
+  return '';
+});
+
+@riverpod
+bool isSendCodeProvider(ref) {
+  return ref.watch(expirationTimeProvider).state.isNotEmpty;
+}
+
+final expirationTimeProvider = StateProvider<String>((ref) {
+  return '';
+});
+
+final codeInputProvider = StateProvider<String>((ref) {
+  return '';
+});
+
+final isCodeVerifiedProvider = StateProvider<bool>((ref) {
+  return false;
+});
+
+final codeInputError = StateProvider<String>((ref) => '');
+
+final withNoErrorsMessagesProvider = StateProvider<bool>((ref) {
+  var errors = [
+    ref.watch(emailErrorProvider),
+    ref.watch(errorPasswordProvider),
+    ref.watch(confirmPasswordErrorProvider),
+    ref.watch(codeInputError),
+    ref.watch(dateOfBirthProvider),
+    ref.watch(errorNameProvider),
+    dateValidator(ref.watch(dateOfBirthProvider)),
+  ];
+
+
+  return errors.every((element) {
+    print('element => $element');
+    return element?.isEmpty ?? false;
+  });
+});
+
+final isSignupProvider = StateProvider<bool>((ref) {
+  return [
+    ref.watch(emailInputProvider),
+    ref.watch(passwordInputProvider),
+    ref.watch(confirmPasswordInputProvider),
+    ref.watch(nameInputProvider),
+    ref.watch(dateOfBirthProvider),
+    ref.watch(phoneInputProvider),
+    ref.watch(codeInputProvider),
+  ].every((element) => element.isNotEmpty);
 });
