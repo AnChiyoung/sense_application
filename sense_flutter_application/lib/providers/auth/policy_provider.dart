5545d7745f7fcd5202b6666c3daae544727388af
reichardcris <reichardcris@gmail.com>
2024-03-15 21:04:28 +0800

    policy provider update policies

--- sense_flutter_application/lib/providers/auth/policy_provider.dart
+++ sense_flutter_application/lib/providers/auth/policy_provider.dart
@@ -12,15 +12,15 @@ class PolicyNotifier extends StateNotifier<List<PolicyAgreement>> {
 
   init() {
     super.state = [
+      // PolicyAgreement(
+      //     title: '전체동의',
+      // ),
       PolicyAgreement(
-          title: '전체동의',
-      ),
-      PolicyAgreement(
-        title: '14세 이상입니다. (필수)',
+        title: '14세 이상입니다. ',
         isRequired: true,
       ),
       PolicyAgreement(
-        title: '센스 이용약관 (필수)',
+        title: '센스 이용약관 ',
         isRequired: true,
         content: const PolicyContent(
           title: '제1조 목적',
@@ -30,17 +30,17 @@ class PolicyNotifier extends StateNotifier<List<PolicyAgreement>> {
         ),
       ),
         PolicyAgreement(
-          title: '개인정보처리동의 (필수)',
+          title: '개인정보처리동의 ',
           isRequired: true,
           content: const PolicyContent(title: '제1조 목적', content: '이 약관은 주식회사 러너스(이하 “회사”라 함)가 운영하는 이벤트 생성 및 광고 플랫폼 센스(sens.im) (이하 “플랫폼”이라 한다)에서 제공하는 큐레이션 및 중개, 판매 서비스(이하 “서비스”라 함)를 이용함에 있어 “회사”와 “이용자”의 권리․의무 및 책임사항을 규정함을 목적으로 합니다.'),
         ),
         PolicyAgreement(
-          title: 'Sprint 1 (필수)',
+          title: '개인정보처리방침 ',
           isRequired: true,
           content: const PolicyContent(title: '제1조 목적', content: '이 약관은 주식회사 러너스(이하 “회사”라 함)가 운영하는 이벤트 생성 및 광고 플랫폼 센스(sens.im) (이하 “플랫폼”이라 한다)에서 제공하는 큐레이션 및 중개, 판매 서비스(이하 “서비스”라 함)를 이용함에 있어 “회사”와 “이용자”의 권리․의무 및 책임사항을 규정함을 목적으로 합니다.'),
         ),
         PolicyAgreement(
-          title: 'Sprint 1 (필수)',
+          title: '마케팅 정보 수신동의 (선택)',
           content: const PolicyContent(title: '제1조 목적', content: '이 약관은 주식회사 러너스(이하 “회사”라 함)가 운영하는 이벤트 생성 및 광고 플랫폼 센스(sens.im) (이하 “플랫폼”이라 한다)에서 제공하는 큐레이션 및 중개, 판매 서비스(이하 “서비스”라 함)를 이용함에 있어 “회사”와 “이용자”의 권리․의무 및 책임사항을 규정함을 목적으로 합니다.'),
         )
     ];
