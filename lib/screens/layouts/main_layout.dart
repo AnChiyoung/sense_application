5a186c745a381d503fecce400634b90c82a5019a
reichardcris <reichardcris@gmail.com>
2024-03-25 21:00:13 +0800

    Update bottom navigation icon and label added user icon

--- lib/screens/layouts/main_layout.dart
+++ lib/screens/layouts/main_layout.dart
@@ -5,6 +5,7 @@ import 'package:sense_flutter_application/screens/widgets/app_bar/top_nav_bar.da
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sense_flutter_application/utils/color_scheme.dart';
 
 
 class MainLayout extends ConsumerWidget {
@@ -36,7 +37,7 @@ class MainLayout extends ConsumerWidget {
                   GoRouter.of(context).go('/login');
                   break;
                 case 2:
-                  GoRouter.of(context).go('/schedule');
+                  GoRouter.of(context).go('/list');
                   break;
                 case 3:
                   GoRouter.of(context).go('/store');
@@ -49,23 +50,59 @@ class MainLayout extends ConsumerWidget {
           currentIndex: _screenIndex,
           unselectedFontSize: 12,
           selectedFontSize: 12,
+          // unselectedIconTheme: const IconThemeData(color: Color.fromRGBO(119, 119, 119, 1)),
+          // selectedIconTheme: IconThemeData(color: primaryColor[50]),
           items: 
             [
               BottomNavigationBarItem(
-                icon:  SvgPicture.asset('lib/assets/images/icons/svg/bottom_bar/home.svg', width: 24, height: 24),
+                icon:  
+                  SvgPicture.asset(
+                    'lib/assets/images/icons/svg/bottom_bar/home.svg',
+                    width: 24,
+                    height: 24,
+                    color: _screenIndex == 0 ? primaryColor[50] : const Color.fromRGBO(119, 119, 119, 1)
+                ),
                 label: '홈',
               ),
               BottomNavigationBarItem(
-                icon:  SvgPicture.asset('lib/assets/images/icons/svg/bottom_bar/list.svg', width: 24, height: 24),
+                icon: 
+                  SvgPicture.asset(
+                    'lib/assets/images/icons/svg/bottom_bar/list.svg',
+                    width: 24,
+                    height: 24,
+                    color: _screenIndex == 1 ? primaryColor[50] : const Color.fromRGBO(119, 119, 119, 1)
+                  ),
                 label: '스토리'
               ),
               BottomNavigationBarItem(
-                icon:  SvgPicture.asset('lib/assets/images/icons/svg/bottom_bar/calendar_blank.svg', width: 24, height: 24),
+                icon:  
+                  SvgPicture.asset(
+                    'lib/assets/images/icons/svg/bottom_bar/calendar_blank.svg',
+                    width: 24,
+                    height: 24,
+                    color: _screenIndex == 2 ? primaryColor[50] : const Color.fromRGBO(119, 119, 119, 1)
+                  ),
                 label: '일정'
               ),
               BottomNavigationBarItem(
-                icon:  SvgPicture.asset('lib/assets/images/icons/svg/bottom_bar/storefront.svg', width: 24, height: 24),
-                label: '일정'
+                icon:
+                  SvgPicture.asset(
+                    'lib/assets/images/icons/svg/bottom_bar/storefront.svg',
+                    width: 24,
+                    height: 24,
+                    color: _screenIndex == 3 ? primaryColor[50] : const Color.fromRGBO(119, 119, 119, 1)
+                  ),
+                label: '쇼핑'
+              ),
+              BottomNavigationBarItem(
+                icon:  
+                  SvgPicture.asset(
+                    'lib/assets/images/icons/svg/bottom_bar/user.svg',
+                    width: 24,
+                    height: 24,
+                    color: _screenIndex == 4 ? primaryColor[50] : const Color.fromRGBO(119, 119, 119, 1)
+                  ),
+                  label: '내 정보',
               )
             ],
         ),
