fe6c5c55daa964fc18bdbc855c2a63e77ba77fad
reichardcris <reichardcris@gmail.com>
2024-03-25 20:40:41 +0800

    color scheme

--- lib/utils/color_scheme.dart
+++ lib/utils/color_scheme.dart
@@ -47,11 +47,11 @@ Map<int, Color> secondary2Color = {
   99: const Color.fromRGBO(233, 252, 255, 1),
 };
 
-Map<int, Color> errorColor = {
-  0: const Color.fromRGBO(195, 20, 20, 1),
-  10: const Color.fromRGBO(216, 32, 32, 1),
-  20: const Color.fromRGBO(255, 197, 197, 1),
-  30: const Color.fromRGBO(255, 216, 216, 1),
+const Map<int, Color> errorColor = {
+  0: Color.fromRGBO(195, 20, 20, 1),
+  10: Color.fromRGBO(216, 32, 32, 1),
+  20: Color.fromRGBO(255, 197, 197, 1),
+  30: Color.fromRGBO(255, 216, 216, 1),
 };
 
 Map<int, Color> successColor = {
@@ -61,11 +61,11 @@ Map<int, Color> successColor = {
   30: const Color.fromRGBO(185, 255, 204, 1),
 };
 
-Map<int, Color> warningColor = {
-  0: const Color.fromRGBO(220, 158, 0, 1),
-  10: const Color.fromRGBO(255, 189, 20, 1),
-  20: const Color.fromRGBO(255, 215, 114, 1),
-  30: const Color.fromRGBO(255, 243, 212, 1),
+const Map<int, Color> warningColor = {
+  0: Color.fromRGBO(220, 158, 0, 1),
+  10: Color.fromRGBO(255, 189, 20, 1),
+  20: Color.fromRGBO(255, 215, 114, 1),
+  30: Color.fromRGBO(255, 243, 212, 1),
 };
 
 
