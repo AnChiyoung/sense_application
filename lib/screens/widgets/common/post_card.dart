c81107d4f46b12726804bbb19820a64263d22c9b
reichardcris <reichardcris@gmail.com>
2024-03-27 16:42:05 +0800

    post card update

--- lib/screens/widgets/common/post_card.dart
+++ lib/screens/widgets/common/post_card.dart
@@ -11,37 +11,16 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return 
-      Container(
-        padding: const EdgeInsets.all(20),
-        child: 
-          Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // mainAxisSize: MainAxisSize.min,
-            children: [
-              AspectRatio(
-                aspectRatio: 335/398,
-                child: 
-                  Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(16), topRight: Radius.circular(16)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color.fromRGBO(0, 0, 0, 0.15),
-                          offset: Offset(0, 2), // Shadow position
-                          blurRadius: 4,
-                        )
-                      ],
-                      image: DecorationImage(image: AssetImage('lib/assets/images/card_image.png'), fit: BoxFit.cover)
-                    ),
-                  ),
-              ),
+      Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // mainAxisSize: MainAxisSize.min,
+        children: [
+          AspectRatio(
+            aspectRatio: 335/398,
+            child: 
               Container(
-                // height: 88,
-                width: double.maxFinite,
-                padding: const EdgeInsets.all(20),
                 decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(16), bottomRight: Radius.circular(16)),
-                  color: Color.fromRGBO(238, 238, 238, 1),
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(16), topRight: Radius.circular(16)),
                   boxShadow: [
                     BoxShadow(
                       color: Color.fromRGBO(0, 0, 0, 0.15),
@@ -49,32 +28,49 @@ class PostCard extends StatelessWidget {
                       blurRadius: 4,
                     )
                   ],
+                  image: DecorationImage(image: AssetImage('lib/assets/images/card_image.png'), fit: BoxFit.cover)
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      subject,
-                      style: 
-                        const TextStyle(
-                          color: Color(0xFF151515),
-                          overflow: TextOverflow.ellipsis, fontSize: 16, fontWeight: FontWeight.w700
-                        ),
-                      ),
-                    const SizedBox(height: 6),
-                    Text(
-                      subtext ?? '',
-                      style: 
-                        const TextStyle(
-                          color: Color(0xFF333333),
-                          overflow: TextOverflow.ellipsis, fontSize: 12, fontWeight: FontWeight.w400
-                        ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+              ),
           ),
+          Container(
+            // height: 88,
+            width: double.maxFinite,
+            padding: const EdgeInsets.all(20),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(16), bottomRight: Radius.circular(16)),
+              color: Color.fromRGBO(238, 238, 238, 1),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.15),
+                  offset: Offset(0, 2), // Shadow position
+                  blurRadius: 4,
+                )
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  subject,
+                  style: 
+                    const TextStyle(
+                      color: Color(0xFF151515),
+                      overflow: TextOverflow.ellipsis, fontSize: 16, fontWeight: FontWeight.w700
+                    ),
+                  ),
+                const SizedBox(height: 6),
+                Text(
+                  subtext ?? '',
+                  style: 
+                    const TextStyle(
+                      color: Color(0xFF333333),
+                      overflow: TextOverflow.ellipsis, fontSize: 12, fontWeight: FontWeight.w400
+                    ),
+                ),
+              ],
+            ),
+          )
+        ],
       );
   }
 }
