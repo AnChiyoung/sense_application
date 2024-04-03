ce081acef2399b81860266ca3a016767bf4e1cd7
reichardcris <reichardcris@gmail.com>
2024-04-03 20:57:08 +0800

    sample toast messages and modals

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -12,75 +12,71 @@ class ListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       theme: ThemeData(
         fontFamily: 'Pretendard',
         colorScheme: ColorScheme.light(
           primary: primaryColor[50] ?? Colors.white,
           secondary: secondary1Color[50] ?? Colors.white,
-          onPrimary: primaryColor[50] ??  Colors.white,
+          onPrimary: primaryColor[50] ?? Colors.white,
         ),
       ),
       home: MainLayout(
-      title: '',
-      body: Container(
-        padding: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 0),
-        child: Column(
-          children: [
-            Center(
-              child: ElevatedButton(onPressed: () {
-                CustomModal.showModal(
-                  context,
-                );
-              }, child: Text('Show Modal')),
-            ),
-            const SizedBox(height: 30),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  CustomToast.errorToast(context, 'Invalid system input');
-
-                }, 
-                child: Text('Show Error Toast')),
-            ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  CustomToast.warningToast(context, 'Invalid system input');
-
-                }, 
-                child: Text('Show Warning Toast')),
-            ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  CustomToast.successToast(context, 'Invalid system input');
-
-                }, 
-                child: Text('Show Success Toast')),
-            ),
-          ],
-        )
-      
-      ),
-      floating: 
-        FloatingActionButton(
+        title: '',
+        body: Container(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 0),
+            child: Column(
+              children: [
+                Center(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        CustomModal.showModal(
+                          context,
+                          title: 'Modal',
+                          message: 'Sample message description',
+                          buttonLabel: 'Close/Ok',
+                        );
+                      },
+                      child: const Text('Show Modal')),
+                ),
+                const SizedBox(height: 30),
+                Center(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        CustomToast.errorToast(context, 'Invalid system input');
+                      },
+                      child: const Text('Show Error Toast')),
+                ),
+                Center(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        CustomToast.warningToast(context, 'Invalid system input');
+                      },
+                      child: const Text('Show Warning Toast')),
+                ),
+                Center(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        CustomToast.successToast(context, 'Invalid system input');
+                      },
+                      child: const Text('Show Success Toast')),
+                ),
+              ],
+            )),
+        floating: FloatingActionButton(
           onPressed: () {
-            // 
+            //
           },
           shape: const CircleBorder(),
-          child: 
-            SvgPicture
-              .asset(
-                'lib/assets/images/icons/svg/plus.svg',
-                color: Colors.white,
-                width: 24,
-                height: 24,
-                fit: BoxFit.cover,
-              ),
+          child: SvgPicture.asset(
+            'lib/assets/images/icons/svg/plus.svg',
+            color: Colors.white,
+            width: 24,
+            height: 24,
+            fit: BoxFit.cover,
+          ),
         ),
-    ),
+      ),
     );
   }
 }
