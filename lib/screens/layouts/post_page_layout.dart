1955dcbb3fb43d787f772593c67394e8ff2da0f9
reichardcris <reichardcris@gmail.com>
2024-04-04 20:25:49 +0800

    post_page_layout added nav header title

--- lib/screens/layouts/post_page_layout.dart
+++ lib/screens/layouts/post_page_layout.dart
@@ -39,6 +39,10 @@ class PostPageLayout extends StatelessWidget {
                 }
               }),
           shape: const Border(bottom: BorderSide(width: 1, color: Color(0xFFEEEEEE))),
+          title: const Text(
+            '여자친구의 추억을 담아줄 카메라 BEST 5',
+            style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.w500),
+          ),
         ),
         body: GestureDetector(
           onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
