e2ea4e4069bacfe4f6250f22cd079ac18b47209c
reichardcris <reichardcris@gmail.com>
2024-04-04 18:46:07 +0800

    forgot password verify update toasts

--- lib/screens/widgets/auth/forgot_password_verify.dart
+++ lib/screens/widgets/auth/forgot_password_verify.dart
@@ -7,8 +7,7 @@ import 'package:sense_flutter_application/screens/widgets/common/input_text_fiel
 import 'package:sense_flutter_application/utils/utils.dart';
 import './validator/index.dart';
 
-
-class VerifiCationFields  {
+class VerifiCationFields {
   final String phone;
   final String code;
   final bool isCodeVerified;
@@ -17,26 +16,20 @@ class VerifiCationFields  {
 
   factory VerifiCationFields.fromJson(Map<String, dynamic> json) {
     return VerifiCationFields(
-      phone: json['phone'],
-      code: json['code'],
-      isCodeVerified: json['isCodeVerified']
-    );
+        phone: json['phone'], code: json['code'], isCodeVerified: json['isCodeVerified']);
   }
 }
-class ForgotPasswordVerify extends StatefulWidget {
 
+class ForgotPasswordVerify extends StatefulWidget {
   final Function(VerifiCationFields) onChanged;
 
-  
   const ForgotPasswordVerify({super.key, required this.onChanged});
-  
+
   @override
   State<ForgotPasswordVerify> createState() => _ForgotPasswordVerifyState();
 }
 
 class _ForgotPasswordVerifyState extends State<ForgotPasswordVerify> {
-
-  
   String code = '';
   String errorMessages = '';
   String phone = '';
@@ -47,117 +40,129 @@ class _ForgotPasswordVerifyState extends State<ForgotPasswordVerify> {
   @override
   void setState(VoidCallback fn) {
     // TODO: implement setState
-     super.setState(fn);
-     widget.onChanged(VerifiCationFields(phone: phone, code: code, isCodeVerified: isCodeVerified));
+    super.setState(fn);
+    widget.onChanged(VerifiCationFields(phone: phone, code: code, isCodeVerified: isCodeVerified));
   }
 
   @override
   Widget build(BuildContext context) {
-    int screenWidth =  MediaQuery.of(context).size.width.toInt(); 
+    int screenWidth = MediaQuery.of(context).size.width.toInt();
     var onChanged = debounce<String>((String value) {
-        setState(() {
-          errorMessages = PhoneValidator.validate(value) ?? '';
-          phone = value;
-        });
-      }, const Duration(milliseconds: 500));
+      setState(() {
+        errorMessages = PhoneValidator.validate(value) ?? '';
+        phone = value;
+      });
+    }, const Duration(milliseconds: 500));
 
-    return 
-      Container(
-        padding: const EdgeInsets.only(bottom: 20, left: 0, right: 0, top: 0),
-        constraints: BoxConstraints(
-          maxWidth: screenWidth > 768 ?  500 : double.infinity,
-        ),
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
+    return Container(
+      padding: const EdgeInsets.only(bottom: 20, left: 0, right: 0, top: 0),
+      constraints: BoxConstraints(
+        maxWidth: screenWidth > 768 ? 500 : double.infinity,
+      ),
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
                   child: InputTextField(
-                    height: 40,
-                    label: '연락처',
-                    onChanged: (String value) {
-                      onChanged(value);
-                    },
-                    placeholder: '010-1234-5678',
-                    mask: [phoneMask],
-                    errorMessage: errorMessages,
-                    append: SizedBox(
-                      width: 97,
-                      child: CustomButton(
-                        labelText: phone.isNotEmpty && errorMessages.isEmpty && isVerification ? '재발송' : '인증받기',
-                        backgroundColor: phone.isNotEmpty && errorMessages.isEmpty ? const Color(0XFF555555) : const Color(0XFFBBBBBB),
-                        textColor: Colors.white,
-                        onPressed: () async {
-                          AuthApi().sendCode(phone).then((value) {
-                            setState(() {
-                              expirationTime = value['data']['expired'];
-                              isVerification = true;
-                            });
-
-                            // Message
-                            String message = phone.isNotEmpty && errorMessages.isEmpty ? '인증번호를 재발송 했어요' :'인증번호가 발송되었습니다';
-                            // Toast
-                            CustomToast.successToast(context, message, bottom: MediaQuery.of(context).size.height * 0.12);
-                          }).catchError((error) {
-                            CustomToast.errorToast(context, error['message'], bottom: MediaQuery.of(context).size.height * 0.12);
-                            isVerification = false;
+                height: 40,
+                label: '연락처',
+                onChanged: (String value) {
+                  onChanged(value);
+                },
+                placeholder: '010-1234-5678',
+                mask: [phoneMask],
+                errorMessage: errorMessages,
+                append: SizedBox(
+                    width: 97,
+                    child: CustomButton(
+                      labelText: phone.isNotEmpty && errorMessages.isEmpty && isVerification
+                          ? '재발송'
+                          : '인증받기',
+                      backgroundColor: phone.isNotEmpty && errorMessages.isEmpty
+                          ? const Color(0XFF555555)
+                          : const Color(0XFFBBBBBB),
+                      textColor: Colors.white,
+                      onPressed: () async {
+                        AuthApi().sendCode(phone).then((value) {
+                          setState(() {
+                            expirationTime = value['data']['expired'];
+                            isVerification = true;
                           });
-                        },
-                      )
-                    ),
-                  )
 
-                ),
-
-              ],
-            ),
-            if (isVerification)
-                Column(
-                  children: [
-                    const SizedBox(height: 24),
-                    InputTextField(
-                      height: 40,
-                      label: '인증번호',
-                      onChanged: (String value) {
-                        setState(() {
-                          code = value;
+                          // Message
+                          String message = phone.isNotEmpty && errorMessages.isEmpty
+                              ? '인증번호를 재발송 했어요'
+                              : '인증번호가 발송되었습니다';
+                          // Toast
+                          CustomToast.successToast(context, message,
+                              bottom: MediaQuery.of(context).size.height * 0.12);
+                        }).catchError((error) {
+                          CustomToast.errorToast(context, error['message'],
+                              bottom: MediaQuery.of(context).size.height * 0.12);
+                          isVerification = false;
                         });
                       },
-                      suffixIcon: isCodeVerified 
-                        ? const Icon(Icons.done, color: Colors.green, size: 20,) 
-                        : CountDownTimer(endTime: expirationTime),
-                      placeholder: '',
-                      errorMessage: '',
-                      append: SizedBox(
-                        width: 97,
-                        child: CustomButton(
-                          labelText: '확인',
-                          backgroundColor: code.isNotEmpty ? const Color(0XFF555555) : const Color(0XFFBBBBBB),
-                          textColor: Colors.white,
-                          onPressed: () async {
-                            AuthApi().verifyCode(phone, code).then((value) {
-                              if (value['code'] == 200) {
-                                setState(() {
-                                isCodeVerified = true;
-                                CustomToast.successToast(context, '인증코드가 확인되었습니다', bottom: MediaQuery.of(context).size.height * 0.12);
-                              });
-                              }
-                              
-                            }).catchError((error) {
-                              CustomToast.errorToast(context, error['message'], bottom: MediaQuery.of(context).size.height * 0.12);
-                              print(error);
-                            });
-                          },
+                    )),
+              )),
+            ],
+          ),
+          if (isVerification)
+            Column(
+              children: [
+                const SizedBox(height: 24),
+                InputTextField(
+                  height: 40,
+                  label: '인증번호',
+                  onChanged: (String value) {
+                    setState(() {
+                      code = value;
+                    });
+                  },
+                  suffixIcon: isCodeVerified
+                      ? const Icon(
+                          Icons.done,
+                          color: Colors.green,
+                          size: 20,
                         )
-                      ),
-                    )
-                  ],
+                      : CountDownTimer(endTime: expirationTime),
+                  placeholder: '',
+                  errorMessage: '',
+                  append: SizedBox(
+                      width: 97,
+                      child: CustomButton(
+                        labelText: '확인',
+                        backgroundColor:
+                            code.isNotEmpty ? const Color(0XFF555555) : const Color(0XFFBBBBBB),
+                        textColor: Colors.white,
+                        onPressed: () async {
+                          AuthApi().verifyCode(phone, code).then((value) {
+                            print('value: $value');
+                            if (value['code'] == 200) {
+                              setState(() {
+                                isCodeVerified = true;
+                                CustomToast.successToast(context, '인증코드가 확인되었습니다',
+                                    bottom: MediaQuery.of(context).size.height * 0.12);
+                              });
+                            } else {
+                              CustomToast.errorToast(context, value['message'],
+                                  bottom: MediaQuery.of(context).size.height * 0.12);
+                            }
+                          }).catchError((error) {
+                            print('error code: $error');
+                            CustomToast.errorToast(context, error['message'],
+                                bottom: MediaQuery.of(context).size.height * 0.12);
+                          });
+                        },
+                      )),
                 )
-          ],
-        ),
-      );
+              ],
+            )
+        ],
+      ),
+    );
   }
 }
