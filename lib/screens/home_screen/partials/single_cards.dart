b625e85a26bf67d5cff021c88eea6b30e30c6f4d
reichardcris <reichardcris@gmail.com>
2024-04-04 18:43:19 +0800

    Implement Consumer widget on single cards -consume the post api data

--- lib/screens/home_screen/partials/single_cards.dart
+++ lib/screens/home_screen/partials/single_cards.dart
@@ -1,30 +1,34 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:sense_flutter_application/screens/widgets/common/post_card.dart';
+import 'package:sense_flutter_application/store/providers/Post/post_collection_provider.dart';
 
-class SingleCards extends StatelessWidget {
-
+class SingleCards extends ConsumerWidget {
   const SingleCards({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final postCollection = ref.watch(postCollectionProvider);
+    List<dynamic> data = postCollection['data'] ?? [];
+    List<Widget> postWidgets = data.map((post) {
+      return Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: PostCard(
+            onTap: () {
+              GoRouter.of(context).go('/home/post/${post['id']}');
+            },
+            subject: post['title'],
+            subtext: post['sub_title'],
+            thumbnail: post['thumbnail_media_url'],
+          ));
+    }).toList();
+
     return Container(
       padding: const EdgeInsets.only(left: 20, right: 20),
       margin: const EdgeInsets.only(top: 16),
-      constraints: const BoxConstraints(
-        maxWidth: 500
-      ),
-      child: Column(
-        children: [
-        ...List.generate(10, 
-          (index) => Padding(
-            padding: const EdgeInsets.only(bottom: 20),
-            child: PostCard(
-              subject: '향기 가득한 입생로랑 리브르 르 퍼퓸 - ${index + 1}',
-              subtext: '향기 가득한 입생로랑 리브르 르 퍼퓸 외 5종',)
-            ),
-          )
-      ],),
+      constraints: const BoxConstraints(maxWidth: 500),
+      child: Column(children: postWidgets),
     );
   }
 }
