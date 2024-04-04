603eeb62a024db733a52567acc601177fe3fbe4c
reichardcris <reichardcris@gmail.com>
2024-04-04 18:47:41 +0800

    PostCard widget added callback onTap

--- lib/screens/widgets/common/post_card.dart
+++ lib/screens/widgets/common/post_card.dart
@@ -1,43 +1,47 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/widgets.dart';
 
 class PostCard extends StatelessWidget {
-
   final String subject;
-  final String ?subtext;
+  final String? subtext;
+  final String thumbnail;
+  final VoidCallback onTap;
 
-  const PostCard({super.key, required this.subject, this.subtext});
+  const PostCard(
+      {super.key, required this.subject, this.subtext, this.thumbnail = '', required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return 
-      Column(
+    return InkWell(
+      onTap: onTap,
+      child: Column(
         // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         // mainAxisSize: MainAxisSize.min,
         children: [
           AspectRatio(
-            aspectRatio: 335/398,
-            child: 
-              Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(16), topRight: Radius.circular(16)),
-                  boxShadow: [
+            aspectRatio: 335 / 398,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(16), topRight: Radius.circular(16)),
+                  boxShadow: const [
                     BoxShadow(
                       color: Color.fromRGBO(0, 0, 0, 0.15),
                       offset: Offset(0, 2), // Shadow position
                       blurRadius: 4,
                     )
                   ],
-                  image: DecorationImage(image: AssetImage('lib/assets/images/card_image.png'), fit: BoxFit.cover)
-                ),
-              ),
+                  image: DecorationImage(image: NetworkImage(thumbnail), fit: BoxFit.cover)),
+            ),
           ),
           Container(
             // height: 88,
             width: double.maxFinite,
             padding: const EdgeInsets.all(20),
             decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(16), bottomRight: Radius.circular(16)),
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(16), bottomRight: Radius.circular(16)),
               color: Color.fromRGBO(238, 238, 238, 1),
               boxShadow: [
                 BoxShadow(
@@ -52,25 +56,26 @@ class PostCard extends StatelessWidget {
               children: [
                 Text(
                   subject,
-                  style: 
-                    const TextStyle(
+                  style: const TextStyle(
                       color: Color(0xFF151515),
-                      overflow: TextOverflow.ellipsis, fontSize: 16, fontWeight: FontWeight.w700
-                    ),
-                  ),
+                      overflow: TextOverflow.ellipsis,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700),
+                ),
                 const SizedBox(height: 6),
                 Text(
                   subtext ?? '',
-                  style: 
-                    const TextStyle(
+                  style: const TextStyle(
                       color: Color(0xFF333333),
-                      overflow: TextOverflow.ellipsis, fontSize: 12, fontWeight: FontWeight.w400
-                    ),
+                      overflow: TextOverflow.ellipsis,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400),
                 ),
               ],
             ),
           )
         ],
-      );
+      ),
+    );
   }
 }
