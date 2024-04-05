ca3c2b91ac90ab726a193f8593d26c8e60ca6a34
reichardcris <reichardcris@gmail.com>
2024-04-06 00:11:22 +0800

    Single Post Screen -Added post thumbnail -Added post content header -Added Content Body -Added Store Products

--- lib/screens/single_post_screen/index.dart
+++ lib/screens/single_post_screen/index.dart
@@ -1,16 +1,13 @@
-import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:sense_flutter_application/screens/home_screen/partials/chip_list_tab.dart';
-import 'package:sense_flutter_application/screens/home_screen/partials/grid_cards.dart';
-import 'package:sense_flutter_application/screens/home_screen/partials/single_cards.dart';
-import 'package:sense_flutter_application/screens/layouts/main_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sense_flutter_application/apis/post/post_api.dart';
 import 'package:sense_flutter_application/screens/layouts/post_page_layout.dart';
-import 'package:sense_flutter_application/screens/widgets/banner_area.dart';
+import 'package:sense_flutter_application/screens/single_post_screen/partials/content_body.dart';
+import 'package:sense_flutter_application/screens/single_post_screen/partials/content_header.dart';
+import 'package:sense_flutter_application/screens/single_post_screen/partials/post_thumbnail.dart';
+import 'package:sense_flutter_application/screens/single_post_screen/partials/store_products.dart';
 import 'package:sense_flutter_application/store/providers/Post/post_collection_provider.dart';
 import 'package:sense_flutter_application/utils/color_scheme.dart';
-import 'package:sense_flutter_application/utils/utils.dart';
 
 class SinglePostScreen extends StatefulWidget {
   final int id;
@@ -21,6 +18,10 @@ class SinglePostScreen extends StatefulWidget {
 }
 
 class _SinglePostScreenState extends State<SinglePostScreen> with WidgetsBindingObserver {
+  Future<Map<String, dynamic>> getPost(String id) async {
+    return await PostApi().getPost(id);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -43,93 +44,59 @@ class _SinglePostScreenState extends State<SinglePostScreen> with WidgetsBinding
                 child: SingleChildScrollView(
                     // controller: scrollController,
                     padding: const EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      // mainAxisSize: MainAxisSize.min,
-                      children: [
-                        AspectRatio(
-                          aspectRatio: 375 / 420,
-                          child: Stack(
-                            children: <Widget>[
-                              Container(
-                                decoration: const BoxDecoration(
-                                  image: DecorationImage(
-                                    image: AssetImage('lib/assets/images/cover.jpeg'),
-                                    fit: BoxFit.cover,
-                                  ),
+                    child: FutureBuilder(
+                        future: getPost(widget.id.toString()),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.waiting) {
+                            return SizedBox(
+                              height: MediaQuery.of(context).size.height,
+                              child: Center(
+                                child: CircularProgressIndicator(
+                                  valueColor: AlwaysStoppedAnimation<Color>(
+                                      primaryColor[50] ?? Colors.white),
                                 ),
                               ),
-                              Container(
-                                decoration: const BoxDecoration(
-                                  gradient: LinearGradient(
-                                    begin: Alignment.bottomCenter,
-                                    end: Alignment.topCenter,
-                                    colors: [
-                                      Color.fromRGBO(21, 21, 21, 0.60),
-                                      Color.fromRGBO(21, 21, 21, 0.00),
-                                    ],
-                                  ),
-                                ),
+                            );
+                          }
+
+                          Map<String, dynamic> post = snapshot.data as Map<String, dynamic>;
+
+                          print(post['data']['id']);
+                          print(post['data']['store_products']);
+
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              PostThumbnail(
+                                imageUrl: post['data']['thumbnail_media_url'],
+                                title: post['data']['title'],
+                                subTitle: post['data']['sub_title'],
+                                date: DateTime.parse(post['data']['created'])
+                                    .toString()
+                                    .substring(0, 10)
+                                    .replaceAll('-', '.'),
+                                likeCount: post['data']['like_count'].toString(),
                               ),
-                              Positioned(
-                                  bottom: 20,
-                                  left: 20,
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      const SizedBox(
-                                        child: Text(
-                                          '이번 여름은\n'
-                                          '‘카캉스\' 어때요?',
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 28,
-                                              fontFamily: 'Pretendard',
-                                              fontWeight: FontWeight.w700),
-                                        ),
-                                      ),
-                                      const SizedBox(
-                                        height: 16,
-                                      ),
-                                      const Text('카페에서 보내는 바캉스 스팟 5곳',
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 14,
-                                              fontFamily: 'Pretendard',
-                                              fontWeight: FontWeight.w500)),
-                                      const SizedBox(height: 24),
-                                      IntrinsicHeight(
-                                          child: Row(
-                                        children: [
-                                          const Text('2023.02.01',
-                                              style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 14,
-                                                  fontFamily: 'Pretendard',
-                                                  fontWeight: FontWeight.w500)),
-                                          const VerticalDivider(
-                                            color: Colors.white,
-                                            thickness: 1,
-                                            width: 20,
-                                          ),
-                                          SvgPicture.asset('lib/assets/images/icons/svg/heart.svg',
-                                              width: 16, height: 16, color: Colors.white),
-                                          const SizedBox(width: 4),
-                                          const Text('4.8 M',
-                                              style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 14,
-                                                  fontFamily: 'Pretendard',
-                                                  fontWeight: FontWeight.w500))
-                                        ],
-                                      ))
-                                    ],
-                                  ))
+                              Container(
+                                width: double.infinity,
+                                padding: const EdgeInsets.all(20),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    ContentHeader(
+                                      title: post['data']['content_title'] ?? '',
+                                      startDate: post['data']['start_date'],
+                                      endDate: post['data']['end_date'],
+                                    ),
+                                    ContentBody(body: post['data']['content'] ?? []),
+                                    const StoreProducts()
+                                  ],
+                                ),
+                              )
                             ],
-                          ),
-                        )
-                      ],
-                    ))),
+                          );
+                        }))),
           );
         }),
       ),
