132eef24055ad2caf9035164542aff8d460c66b3
reichardcris <reichardcris@gmail.com>
2024-04-08 17:19:32 +0800

    Refactor existing post,auth api Added AuthService with Authorization Berear _Token headers

--- lib/apis/auth/auth_api.dart
+++ lib/apis/auth/auth_api.dart
@@ -1,57 +1,54 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:sense_flutter_application/service/api_service.dart';
 import 'package:sense_flutter_application/models/user.dart';
 
 class AuthApi {
   Future<Map<String, dynamic>> checkEmail(String email) async {
-    final response = await http.post(
-      Uri.parse('https://server.dev.sens.im/api/v1/user/email/check'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
+    final response = await ApiService.post(
+      'user/email/check',
+      jsonEncode(<String, String>{
         'email': email,
       }),
     );
 
     if (response.statusCode == 200) {
-      return { 'status': true };
+      return {'status': true};
     } else {
       var parse = json.decode(utf8.decode(response.bodyBytes));
       var nonFieldError = parse['errors']['non_field_errors'];
-      return {'status': false, 'message': nonFieldError?.isNotEmpty ? nonFieldError[0] : parse['message'] };
+      return {
+        'status': false,
+        'message': nonFieldError?.isNotEmpty ? nonFieldError[0] : parse['message']
+      };
     }
   }
 
   Future<Map<String, dynamic>> sendCode(String phone) async {
-    final response = await http.post(
-      Uri.parse('https://server.dev.sens.im/api/v1/user/phone/send'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
+    final response = await ApiService.post(
+      'user/phone/send',
+      jsonEncode(<String, String>{
         'phone': phone.replaceAll(RegExp(r'[^0-9]'), ''),
       }),
     );
 
-
-      var parse = json.decode(utf8.decode(response.bodyBytes));
+    var parse = json.decode(utf8.decode(response.bodyBytes));
 
     if (response.statusCode == 200) {
       return parse;
     } else {
       var nonFieldError = parse['errors']['non_field_errors'];
-      return { 'code': response.statusCode, 'status': false, 'message': nonFieldError?.isNotEmpty ? nonFieldError[0] : parse['message'] };
+      return {
+        'code': response.statusCode,
+        'status': false,
+        'message': nonFieldError?.isNotEmpty ? nonFieldError[0] : parse['message']
+      };
     }
   }
 
   Future<Map<String, dynamic>> verifyCode(String phone, String code) async {
-    final response = await http.post(
-      Uri.parse('https://server.dev.sens.im/api/v1/user/phone/auth'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, dynamic>{
+    final response = await ApiService.post(
+      'user/phone/auth',
+      jsonEncode(<String, dynamic>{
         'phone': phone.replaceAll(RegExp(r'[^0-9]'), ''),
         'code': int.parse(code),
       }),
@@ -63,17 +60,18 @@ class AuthApi {
       return parse;
     } else {
       var nonFieldError = parse['errors']['non_field_errors'];
-      return { 'code': response.statusCode, 'status': false, 'message': nonFieldError?.isNotEmpty ? nonFieldError[0] : parse['message'] };
+      return {
+        'code': response.statusCode,
+        'status': false,
+        'message': nonFieldError?.isNotEmpty ? nonFieldError[0] : parse['message']
+      };
     }
   }
 
   Future<Map<String, dynamic>> register(User payload) async {
-    final response = await http.post(
-      Uri.parse('https://server.dev.sens.im/api/v1/user/signup'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, dynamic>{
+    final response = await ApiService.post(
+      'user/signup',
+      jsonEncode(<String, dynamic>{
         'email': payload.email,
         'password': payload.password,
         'birthday': payload.birthday,
@@ -88,17 +86,18 @@ class AuthApi {
       return parse;
     } else {
       var nonFieldError = parse['errors']['non_field_errors'];
-      return { 'code': response.statusCode, 'status': false, 'message': nonFieldError?.isNotEmpty ? nonFieldError[0] : parse['message'] };
+      return {
+        'code': response.statusCode,
+        'status': false,
+        'message': nonFieldError?.isNotEmpty ? nonFieldError[0] : parse['message']
+      };
     }
   }
 
   Future loginUser(String email, String password) async {
-    final response = await http.post(
-      Uri.parse('https://server.dev.sens.im/api/v1/user/login'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, dynamic>{
+    final response = await ApiService.post(
+      'user/login',
+      jsonEncode(<String, dynamic>{
         'email': email,
         'password': password,
       }),
@@ -110,12 +109,9 @@ class AuthApi {
   }
 
   Future loginWithKakao(String accessToken) async {
-    final response = await http.post(
-      Uri.parse('https://server.dev.sens.im/api/v1/kakao/login'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, dynamic>{
+    final response = await ApiService.post(
+      'kakao/login',
+      jsonEncode(<String, dynamic>{
         'access_token': accessToken,
       }),
     );
---
+++ lib/service/api_service.dart
@@ -0,0 +1,38 @@
+import 'package:http/http.dart' as http;
+import 'package:sense_flutter_application/service/auth_service.dart';
+
+class ApiService {
+  static final ApiService _instance = ApiService._internal();
+  factory ApiService() {
+    return _instance;
+  }
+
+  static const String baseUrl = 'https://server.dev.sens.im/api/v1/';
+  static const String authToken = 'YOUR_AUTH_TOKEN';
+  static final Map<String, String> headers = {
+    'Content-Type': 'application/json; charset=UTF-8',
+  };
+
+  ApiService._internal();
+
+  static Future<http.Response> post(String path, dynamic body) async {
+    String authToken = await AuthService.getAccessToken() ?? '';
+    if (authToken.isNotEmpty) {
+      headers['Authorization'] = 'Bearer $authToken';
+    }
+
+    final url = Uri.parse('$baseUrl$path');
+    final response = await http.post(url, headers: headers, body: body);
+    return response;
+  }
+
+  static Future<http.Response> get(String path, {String? fullUrl}) async {
+    String authToken = await AuthService.getAccessToken() ?? '';
+    if (authToken.isNotEmpty) {
+      headers['Authorization'] = 'Bearer $authToken';
+    }
+    final url = Uri.parse(fullUrl ?? '$baseUrl$path');
+    final response = await http.get(url, headers: headers);
+    return response;
+  }
+}
