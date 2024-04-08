fce86226f678aaa5d75450257161b2673358fe23
reichardcris <reichardcris@gmail.com>
2024-04-08 22:48:16 +0800

    Comment section display list of comments with dynamic user name

--- lib/screens/single_post_screen/partials/comment_section.dart
+++ lib/screens/single_post_screen/partials/comment_section.dart
@@ -3,17 +3,37 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sense_flutter_application/apis/post/post_api.dart';
 import 'package:sense_flutter_application/models/comment.dart';
+import 'package:sense_flutter_application/screens/widgets/common/TextIcon.dart';
 import 'package:sense_flutter_application/screens/widgets/common/comment_text_area.dart';
-import 'package:sense_flutter_application/store/providers/Post/post_collection_provider.dart';
+import 'package:sense_flutter_application/store/providers/Post/comment_collection_provider.dart';
+import 'package:sense_flutter_application/utils/color_scheme.dart';
 
 class CommentSection extends ConsumerWidget {
-  final int post_id;
+  final int postId;
   final int commentCount;
 
-  const CommentSection({super.key, required this.post_id, this.commentCount = 0});
+  const CommentSection({super.key, required this.postId, this.commentCount = 0});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue fetcher = ref.watch(futureCommentProvider(postId.toString()));
+    final commentProviders = ref.watch(commentProvider);
+    if (fetcher.isLoading || commentProviders.isEmpty) {
+      return SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Container(
+          color: Colors.white,
+          child: Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(primaryColor[50] ?? Colors.white),
+            ),
+          ),
+        ),
+      );
+    }
+
+    List<dynamic> comments = commentProviders['data'].toList();
+
     return Container(
       padding: const EdgeInsets.only(top: 32),
       decoration: const BoxDecoration(
@@ -54,16 +74,19 @@ class CommentSection extends ConsumerWidget {
           const SizedBox(
             height: 16,
           ),
-          FutureBuilder(
-              future: ref.read(commentProvider.notifier).fetchComments(post_id.toString()),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                return Container();
-              })
+          fetcher.when(
+              data: (_) {
+                return Column(
+                  children: comments
+                      .map((comment) => CommentTile(
+                            content: comment['content'],
+                            user: comment['user'],
+                          ))
+                      .toList(),
+                );
+              },
+              loading: () => const CircularProgressIndicator(),
+              error: (error, stack) => Text('Error: $error')),
         ],
       ),
     );
@@ -71,50 +94,63 @@ class CommentSection extends ConsumerWidget {
 }
 
 class CommentTile extends StatelessWidget {
-  final Comment comment;
+  final Map<String, dynamic> user;
+  final String content;
 
-  const CommentTile({super.key, required this.comment});
+  const CommentTile({super.key, required this.content, required this.user});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                const ProfileAvatar(
-                  size: 32,
-                ),
-                const SizedBox(
-                  width: 8,
-                ),
-                RichText(
-                    text: const TextSpan(
-                        text: '김영하',
-                        children: [
-                          TextSpan(text: ' • 5시간', style: TextStyle(color: Color(0xFFBBBBBB)))
-                        ],
-                        style: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.w500, color: Color(0xFF555555))))
-              ],
-            ),
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.more_horiz_outlined,
-                  color: Color(0xFF555555),
-                ))
-          ],
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Color(0xFFEEEEEE),
+            width: 1,
+          ),
         ),
-        const SizedBox(
-          height: 6,
-        ),
-        Text(comment.text, style: const TextStyle(color: Color(0xFF151515), fontSize: 14))
-      ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  ProfileAvatar(
+                    size: 32,
+                    imageUrl: user['profile_image_url'] ?? '',
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  RichText(
+                      text: TextSpan(
+                          text: '${user['username']}',
+                          children: const [
+                            TextSpan(text: ' • 5시간', style: TextStyle(color: Color(0xFFBBBBBB)))
+                          ],
+                          style: const TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.w500, color: Color(0xFF555555))))
+                ],
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.more_horiz_outlined,
+                    color: Color(0xFF555555),
+                  ))
+            ],
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Text(content, style: const TextStyle(color: Color(0xFF151515), fontSize: 14)),
+          const ActionButtons()
+        ],
+      ),
     );
   }
 }
@@ -124,7 +160,43 @@ class ActionButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(top: 12, bottom: 12),
+      child: IntrinsicHeight(
+        child: Row(
+          children: [
+            const TextIcon(
+              text: '12.5K',
+              iconPath: 'lib/assets/images/icons/svg/like.svg',
+              iconColor: Color(0xFFBBBBBB),
+              iconSize: 16,
+              spacing: 8,
+              textStyle: TextStyle(color: Color(0xFF555555), fontSize: 12),
+            ),
+            const SizedBox(width: 12),
+            const TextIcon(
+              text: '1',
+              iconPath: 'lib/assets/images/icons/svg/chat.svg',
+              iconColor: Color(0xFFBBBBBB),
+              iconSize: 16,
+              spacing: 8,
+              textStyle: TextStyle(color: Color(0xFF555555), fontSize: 12),
+            ),
+            const VerticalDivider(
+              color: Color(0xFFEEEEEE),
+              thickness: 1,
+              width: 20,
+            ),
+            InkWell(
+              onTap: () {
+                print('reply comment');
+              },
+              child: const Text('답글 달기'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
