50ffeb44ed79b9263ce17d7be297a1175a67f32e
reichardcris <reichardcris@gmail.com>
2024-04-11 19:58:34 +0800

    Collection provider for comment section

--- lib/store/providers/Post/comment_collection_provider.dart
+++ lib/store/providers/Post/comment_collection_provider.dart
@@ -1,7 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sense_flutter_application/apis/post/post_api.dart';
+import 'package:sense_flutter_application/service/api_service.dart';
 
 final futureCommentProvider = FutureProvider.autoDispose.family<void, String>((ref, postId) async {
+  return ref.read(commentProvider.notifier).fetchComments(postId);
+});
+
+final futureCommentNextProvider =
+    FutureProvider.autoDispose.family<void, String>((ref, postId) async {
   return ref.read(commentProvider.notifier).fetchComments(postId);
 });
 
@@ -24,9 +32,34 @@ class CommentCollection extends StateNotifier<Map<String, dynamic>> {
     }
   }
 
+  Future<void> loadMoreComments() async {
+    if (state['next'] != null) {
+      ApiService.get('', fullUrl: state['next']).then((response) {
+        var parse = json.decode(utf8.decode(response.bodyBytes));
+        setComments = {
+          ...parse,
+          ...{
+            'data': [...state['data'], ...parse['data']]
+          }
+        };
+      });
+    }
+  }
+
   void addComment(Map<String, dynamic> comment) {
     List<dynamic> comments = state['data'];
     comments.insert(0, comment['data']);
+    setComments = {
+      ...state,
+      ...{'data': comments}
+    };
+  }
+
+  void addChildComment(Map<String, dynamic> comment) {
+    List<dynamic> comments = state['data'];
+    int parentIndex = comments.indexWhere((element) => element['id'] == comment['id']);
+    comments[parentIndex] = comment;
+
     setComments = {
       ...state,
       ...{'data': comments}
