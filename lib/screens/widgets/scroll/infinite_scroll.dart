d46bc5e308c144423110b03ee9a05df4382b72eb
reichardcris <reichardcris@gmail.com>
2024-04-22 12:33:36 +0800

    home screen fix scrolling widget

--- lib/screens/widgets/scroll/infinite_scroll.dart
+++ lib/screens/widgets/scroll/infinite_scroll.dart
@@ -34,14 +34,13 @@ class _InfiniteScrollState extends State<InfiniteScroll> {
 
   void _scrollListener() {
     if (_scrollController.position.outOfRange ||
-        _scrollController.position.pixels >= _scrollController.position.maxScrollExtent &&
-            _scrollController.position.userScrollDirection == ScrollDirection.reverse) {
+        _scrollController.position.pixels >= _scrollController.position.maxScrollExtent) {
       _fetchData();
     }
 
-    if (!widget.isLoading) {
-      widget.onScroll(_scrollController.position.userScrollDirection == ScrollDirection.forward);
-    }
+    // if (!widget.isLoading) {
+    //   widget.onScroll(_scrollController.position.userScrollDirection == ScrollDirection.forward);
+    // }
   }
 
   Future<void> _fetchData() async {
@@ -52,17 +51,48 @@ class _InfiniteScrollState extends State<InfiniteScroll> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        controller: _scrollController,
-        child: Column(
-          children: [
-            widget.child,
-            if (widget.isLoading)
-              const Padding(
-                padding: EdgeInsets.only(top: 16, bottom: 16),
-                child: CircularProgressIndicator(),
-              ),
-          ],
-        ));
+    return GestureDetector(
+      onVerticalDragStart: (details) => {widget.onScroll(false)},
+      onVerticalDragUpdate: (details) {
+        // Calculate the new position immediately in response to the drag.
+        double scale = 2.0; // This scale factor can be adjusted to control sensitivity
+        double newPosition = _scrollController.position.pixels - details.primaryDelta! * scale;
+        _scrollController
+            .jumpTo(newPosition.clamp(0.0, _scrollController.position.maxScrollExtent));
+      },
+      onVerticalDragEnd: (details) {
+        // When the drag ends, we apply some additional inertia
+        double velocity =
+            details.primaryVelocity! * 0.2; // Adjust the multiplier to control the inertia effect
+        double targetPosition = _scrollController.position.pixels - velocity;
+
+        if (targetPosition < 0 || targetPosition > _scrollController.position.maxScrollExtent) {
+          // If the target position is out of range, use a bounce effect.
+          targetPosition = targetPosition.clamp(0.0, _scrollController.position.maxScrollExtent);
+        }
+
+        _scrollController.animateTo(
+          targetPosition,
+          duration:
+              const Duration(milliseconds: 500), // Adjust timing to simulate natural deceleration
+          curve: Curves.decelerate, // This curve gives a natural "slow down" effect
+        );
+
+        widget.onScroll(true);
+      },
+      child: SingleChildScrollView(
+          controller: _scrollController,
+          physics: const ScrollPhysics(parent: NeverScrollableScrollPhysics()),
+          child: Column(
+            children: [
+              widget.child,
+              if (widget.isLoading)
+                const Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 16),
+                  child: CircularProgressIndicator(),
+                ),
+            ],
+          )),
+    );
   }
 }
