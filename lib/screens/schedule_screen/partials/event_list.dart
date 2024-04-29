78230474dce5cc2f5736262f6e58f1d51d66aeeb
reichardcris <reichardcris@gmail.com>
2024-04-29 18:07:12 +0800

    Added empty event list

--- lib/screens/schedule_screen/partials/event_list.dart
+++ lib/screens/schedule_screen/partials/event_list.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 import 'package:sense_flutter_application/screens/widgets/common/event_tile.dart';
 import 'package:sense_flutter_application/store/providers/Schedule/event_collection.dart';
 import 'package:sense_flutter_application/utils/color_scheme.dart';
 
 class EventList extends ConsumerWidget {
-  const EventList({super.key});
+  final ScrollController controller;
+  const EventList({super.key, required this.controller});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -21,8 +23,44 @@ class EventList extends ConsumerWidget {
       );
     }
 
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
+    if (events.isEmpty) {
+      return CustomScrollView(
+          // animation: controller,
+          controller: controller,
+          slivers: [
+            SliverFillRemaining(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      'lib/assets/images/icons/svg/warning_circle.svg',
+                      width: 40,
+                      height: 40,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    const Text(
+                      '아직 이벤트가 없습니다.',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xFF999999),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ]);
+    }
+
+    return SingleChildScrollView(
+      padding: const EdgeInsets.only(top: 8, right: 20, left: 20),
+      physics: const NeverScrollableScrollPhysics(),
+      controller: controller,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
